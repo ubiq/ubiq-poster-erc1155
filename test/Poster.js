4a11d15f0f9ea62ec64dba1f52c568e67a1766dc
Julian Yap <julianokyap@gmail.com>
2020-12-30 23:56:32 -1000

    Rewrite test and add PosterShop deployment

--- test/Poster.js
+++ test/Poster.js
@@ -1,96 +1,87 @@
 const Poster = artifacts.require("Poster");
-//const PosterShop = artifacts.require("PosterShop");
+const PosterShop = artifacts.require("PosterShop");
 
-contract("Poster", accounts => {
-    it("should put 100 Poster ID 0 in the first account", () =>
-        Poster.deployed()
-            .then(instance => instance.balanceOf.call(accounts[0], 0))
-            .then(balance => {
-                assert.equal(
-                    balance.toNumber(),
-                    100,
-                    "100 wasn't in the first account"
-                );
-            }));
+let user0;
+let user1;
+let user2;
+let user3;
+let posterContract;
+let posterShopContract;
 
-    it("should send 10 Poster ID 0 correctly", () => {
-        let poster;
-
-        // Get initial balances of first and second account.
-        const account_one = accounts[0];
-        const account_two = accounts[1];
-
-        let account_one_starting_balance;
-        let account_two_starting_balance;
-        let account_one_ending_balance;
-        let account_two_ending_balance;
-
-        const amount = 10;
-
-        return Poster.deployed()
-            .then(instance => {
-                poster = instance;
-                return poster.balanceOf.call(account_one, 0);
-            })
-            .then(balance => {
-                account_one_starting_balance = balance.toNumber();
-                return poster.balanceOf.call(account_two, 0);
-            })
-            .then(balance => {
-                account_two_starting_balance = balance.toNumber();
-                return poster.safeTransferFrom(account_one, account_two, 0, amount, "0x0");
-            })
-            .then(() => poster.balanceOf.call(account_one, 0))
-            .then(balance => {
-                account_one_ending_balance = balance.toNumber();
-                return poster.balanceOf.call(account_two, 0);
-            })
-            .then(balance => {
-                account_two_ending_balance = balance.toNumber();
-
-                assert.equal(
-                    account_one_ending_balance,
-                    account_one_starting_balance - amount,
-                    "Amount wasn't correctly taken from the sender"
-                );
-                assert.equal(
-                    account_two_ending_balance,
-                    account_two_starting_balance + amount,
-                    "Amount wasn't correctly sent to the receiver"
-                );
-            });
+contract("Poster and PosterShop", accounts => {
+    before(async () => {
+        user0 = accounts[0];
+        user1 = accounts[1];
+        user2 = accounts[2];
+        user3 = accounts[3];
+        rate = 5000000000000000000n
+        posterContract = await Poster.new();
+        posterShopContract = await PosterShop.new(rate, user0, posterContract.address, 0, user0);
     });
 
-    it("should burn 10 Poster ID 0 correctly", () => {
-        let poster;
+    it("should put 100 Poster ID 0 in the first account", async () => {
+        let balance = await posterContract.balanceOf.call(user0, 0)
 
-        // Get initial balance
-        const account_one = accounts[0];
+        assert.equal(
+            balance.toNumber(),
+            100,
+            "100 wasn't in the first account"
+        );
+    });
 
-        let account_one_starting_balance;
-        let account_one_ending_balance;
-
+    it("should send 10 Poster ID 0 correctly", async () => {
         const amount = 10;
 
-        return Poster.deployed()
-            .then(instance => {
-                poster = instance;
-                return poster.balanceOf.call(account_one, 0);
-            })
-            .then(balance => {
-                account_one_starting_balance = balance.toNumber();
-                return poster.burn(account_one, 0, amount)
-            })
-            .then(() => poster.balanceOf.call(account_one, 0))
-            .then(balance => {
-                account_one_ending_balance = balance.toNumber();
+        balance = await posterContract.balanceOf.call(user0, 0)
+        let user0_start_balance = balance.toNumber();
+        balance = await posterContract.balanceOf.call(user1, 0)
+        let user1_start_balance = balance.toNumber();
 
-                assert.equal(
-                    account_one_ending_balance,
-                    account_one_starting_balance - amount,
-                    "Amount wasn't correctly burnt"
-                );
-            });
+        await posterContract.safeTransferFrom(user0, user1, 0, amount, "0x0")
+
+        balance = await posterContract.balanceOf.call(user0, 0)
+        let user0_end_balance = balance.toNumber();
+        balance = await posterContract.balanceOf.call(user1, 0)
+        let user1_end_balance = balance.toNumber();
+
+        assert.equal(
+            user0_end_balance,
+            user0_start_balance - amount,
+            "Amount wasn't correctly taken from the sender"
+        );
+        assert.equal(
+            user1_end_balance,
+            user1_start_balance + amount,
+            "Amount wasn't correctly sent to the receiver"
+        );
+    });
+
+    it("should burn 10 Poster ID 0 correctly", async () => {
+        const amount = 10;
+
+        balance = await posterContract.balanceOf.call(user0, 0)
+        let user0_start_balance = balance.toNumber();
+
+        await posterContract.burn(user0, 0, amount)
+
+        balance = await posterContract.balanceOf.call(user0, 0)
+        let user0_end_balance = balance.toNumber();
+
+        assert.equal(
+            user0_end_balance,
+            user0_start_balance - amount,
+            "Amount wasn't correctly burnt"
+        );
+    });
+
+    it('has the correct Poster Shop rate', async () => {
+        let rate = await posterShopContract.rate()
+
+        assert.equal(
+            rate.toString(),
+            "5000000000000000000",
+            "Poster Shop rate is not right"
+        );
     });
 
 })
