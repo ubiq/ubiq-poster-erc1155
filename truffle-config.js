297fb65636ae0193298f09d5922c53d133aed312
Julian Yap <julianokyap@gmail.com>
2022-01-28 21:34:59 -0800

    Update gas deployment

--- truffle-config.js
+++ truffle-config.js
@@ -36,42 +36,24 @@ module.exports = {
    */
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    },
+    mainnet: {
+      host: "127.0.0.1",
+      port: 8588,
+      network_id: 8,
+      websockets: false,
+      gas: 6000000,
+      gasPrice: 81000000000,
+      confirmations: 1,
+      timeoutBlocks: 200,
+      from: "0xc5070A5CB93F4497240a57969485C0FbF5c2ee3A",
+      skipDryRun: false
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -88,7 +70,7 @@ module.exports = {
          enabled: true,
          runs: 200
        },
-       evmVersion: "istanbul"
+       evmVersion: "london"
       }
     }
   }
